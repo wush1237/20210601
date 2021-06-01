a83ce304a46a430c6134637af0c8c31e3dd9cda8
wush1237 <wush0913@gmail.com>
2021-06-01 15:04:42 +0800

    CarWin set

---
+++ src/main/java/com/Car/CarWin.java
@@ -0,0 +1,17 @@
+package com.Car;
+
+import javax.swing.*;
+
+public class CarWin {
+    
+    static final int WIDTH = 800;
+    static final int HEIGHT = 500;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(WIDTH,HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
--- src/main/java/com/Car/Tester.java
+++ src/main/java/com/Car/Tester.java
@@ -1,8 +1,31 @@
 package com.Car;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class Tester {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello2");
+        int fee = 30;
+        Map<String, Car> cars = new HashMap<>();
+        Car c1 = new Car("abc-123", "07:30");
+        Car c2 = new Car("xyz-123", "09:45");
+        Car c3 = new Car("ghi-123", "10:30");
+        cars.put(c1.id, c1);
+        cars.put(c2.id, c2);
+        cars.put(c3.id, c3);
+        String id = "abc-123";
+        Car car = cars.get(id);
+        if (car == null) {
+            System.out.println("Car not found");
+        } else {
+            Date now = new Date();
+            long ms = now.getTime() - car.enter.getTime();
+            long mins = ms/(1000*60);
+            System.out.println(mins);
+        }
+
     }
 }
