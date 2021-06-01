7aa7c874a81b0a182d2408d32a1029796f5e5e1d
wush1237 <wush0913@gmail.com>
2021-06-01 16:05:49 +0800

    CarDemo test2

--- src/main/java/com/Car/CarDemo.java
+++ src/main/java/com/Car/CarDemo.java
@@ -3,6 +3,18 @@ package com.Car;
 import javax.swing.*;
 
 public class CarDemo {
-    private JPanel panel1;
-    private JButton okButton;
+    private JPanel mainPanel;
+    private JPanel northPanel;
+    private JButton button1;
+    private JTextField textField1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setContentPane(new CarDemo().mainPanel);
+        frame.setSize(800,500);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
 }
+
